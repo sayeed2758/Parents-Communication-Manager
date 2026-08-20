ee24910ff98dc29e862fca94121f30b0ebabbf0f
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-20 19:09:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='pcm-pro-v2-cache';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/css/style.css','./assets/js/app.js','./assets/js/storage.js','./assets/js/utils.js','./assets/js/integrations.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))))});
+const CACHE="pcm-pro-v1";const ASSETS=["./","./index.html","./assets/css/style.css","./assets/js/app.js","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
