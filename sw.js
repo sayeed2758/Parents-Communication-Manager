50df10027d0a7781248fff37cc6c8c3e017cd03f
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-20 20:22:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="pcm-v2-fixed";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
+const CACHE="pcm-pro-v2-advanced";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./assets/css/style.css","./assets/js/app.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
